33cb1fe94543675c3097dd371d2fe51a7bd86b71
Luis Omar Emilio Perez Rodriguez <luis_perez6@ucne.edu.do>
2018-10-06 18:51:11 -0400

    Entidad Hecha

---
+++ RegistroDetalle/Entidades/TelefonosDetalle.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace RegistroDetalle.Entidades
+{
+    internal class TelefonosDetalle
+    {
+        [key]
+        public int Id { get; set; }
+        public int PersonaId { get; set; }
+        public string TipoTelefono { get; set; }
+        public string Telefono { get; set; }
+
+        public TelefonosDetalle(int id, int personaId, string tipoTelefono, string telefono)
+        {
+            Id = 0;
+            PersonaId = 0;
+            TipoTelefono = string.Empty;
+            Telefono = string.Empty;
+        }
+    }
+}
