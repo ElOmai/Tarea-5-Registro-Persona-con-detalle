3e1a0fade70df2f15f790b267c60916a88cbf997
Luis Omar Emilio Perez Rodriguez <luis_perez6@ucne.edu.do>
2018-10-06 21:55:20 -0400

    ei finai

--- RegistroDetalle/Program.cs
+++ RegistroDetalle/Program.cs
@@ -8,9 +8,6 @@ namespace RegistroDetalle
 {
     static class Program
     {
-        /// <summary>
-        /// The main entry point for the application.
-        /// </summary>
         [STAThread]
         static void Main()
         {
